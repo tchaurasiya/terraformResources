52a512129b0df520f9bff3238dce3e3fbf5b3406
Tathagat Chaurasiya <tathagat.chaurasiya@harness.io>
2021-03-17 11:17:07 +0530

    aws config files with variables

---
+++ aws-with-vars/config.tf
@@ -0,0 +1,26 @@
+variable "access_key" {}
+
+variable "secret_key" {}
+
+
+variable "tag" {
+  default = "tathagat-test-1"
+}
+
+provider "aws" {
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region = "us-east-1"
+}
+
+resource "aws_instance" "ec2_instance_test" {
+  ami           = "ami-0663143d1f1caa3bf"
+  instance_type = "t2.micro"
+  tags = {
+    Name = var.tag
+  }
+}
+
+output "tag" {
+  value = var.tag
+}
